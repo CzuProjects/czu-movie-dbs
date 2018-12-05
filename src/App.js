7658c07e8099463eb6781786de2821e9f90a5757
Michal Siron <michal.siron@merck.com>
2018-12-05 15:16:52 +0100

    Interceptors for request and response

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import './App.css';
 
 import Layout from './components/Layout/Layout';
 import MovieDbs from './containers/MovieDbs/MovieDbs';
+import axios from 'axios';
+
+
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return Promise.reject(request);
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return Promise.reject(response);
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 class App extends Component {
   render() {
