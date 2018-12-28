0aea922d9618c2875360f953f6312cf423e60ae0
Michal Siron <michal.siron@merck.com>
2018-12-28 13:46:07 +0100

    Updates

---
+++ precache-manifest.9c6007cce0850b68a1dbd37d01b0d838.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc43aefb2cb9ff87c26e",
+    "url": "/static/js/main.cc43aefb.chunk.js"
+  },
+  {
+    "revision": "f90de51aabaa05d8087e",
+    "url": "/static/js/1.f90de51a.chunk.js"
+  },
+  {
+    "revision": "cc43aefb2cb9ff87c26e",
+    "url": "/static/css/main.de0e770f.chunk.css"
+  },
+  {
+    "revision": "f2b96e82fc3062a2ccb3ff8969b0868f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/czu-movie-dbs/precache-manifest.002d358f43b5d558a4fbb9a8d0d39742.js"
+  "/precache-manifest.9c6007cce0850b68a1dbd37d01b0d838.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/czu-movie-dbs/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
